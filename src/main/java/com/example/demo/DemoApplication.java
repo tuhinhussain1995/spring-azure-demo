5c9b8410df4663cc7740bfe191e977ce59ed310f
tuhinhussain1995 <145957894+tuhin-hashcode@users.noreply.github.com>
2024-01-17 16:39:52 +0800

    added java 17

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+
+//JPA
+//Spring Starter WEB
+//MySQL
+
+//Just need to create a DB calls usersdb
+
+
+// use usersDB;
+// create table users(id int, first_name varchar(30), last_name varchar(20), email varchar(20));
